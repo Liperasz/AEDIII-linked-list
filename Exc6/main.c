cd957080140873aa2b597f390ccd1d31b0822923
Liperasz <fefonsecaa14@gmail.com>
2025-05-15 21:58:13 -0300

    Atualizating the main of the Exc8

--- Exc6/main.c
+++ Exc6/main.c
@@ -7,5 +7,30 @@
 
 int main() {
 
+    int vet[18] = {1, 5, 2, 5, 3, 5, 4, 5, 6, 5, 7, 5, 8, 5, 9, 5, 10, 5};
+    int vet2[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    List *list = Create_List();
+    List *list2 = Create_List();
+
+    for (int i = 0; i < 18; i++) {
+        Insert_Last_List(list, vet[i]);
+    }
+
+    Print_List(list);
+    printf("\n\n\n\n\n");
+
+    for (int i = 0; i < 15; i++) {
+        Insert_First_List(list2, vet2[i]);
+    }
+    Print_List(list2);
+    printf("\n\n\n\n\n");
+
+
+    Concatenate_List(list, list2);
+    Print_List(list);
+
+    Free_List(list);
+
     return 0;
 }
